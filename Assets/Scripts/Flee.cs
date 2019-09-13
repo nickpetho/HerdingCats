103ae2384e53edee7c039a2718eeae8c84e95de1
Nick Petho <nick.petho@gmail.com>
2019-09-13 09:36:49 -0400

    Initial Commit
    Added assets, scripts, and the scene.

---
+++ Assets/Scripts/Flee.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script moves the object away from another object with a given tag
+public class Flee : MonoBehaviour
+{
+
+    Rigidbody2D rb; // Rigidbody physics component
+
+    public string targettag = "Player";
+    public float speed = 0.01f;
+
+    void Start()
+    {
+        // Get the Rigidbody component of this object
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Move away from location of target object
+    void Update()
+    {
+
+        // Get the target object with this tag
+        GameObject targetObject = GameObject.FindWithTag(targettag);
+
+        // If object not found, this will be null. Exit to prevent error.
+        if (targetObject == null)
+        {
+            return;
+        }
+
+        // Get its Rigidboy component so can get its position
+        Rigidbody2D targetRb = targetObject.GetComponent<Rigidbody2D>();
+
+        // Get difference between current and target location 
+        Vector2 delta = targetRb.position - rb.position;
+
+        // Normalize to 1 
+        delta.Normalize();
+
+        // Multiply be speed
+        delta = delta * speed;
+
+        // Use this to change position
+        rb.position -= delta;
+
+    }
+}
---
+++ Assets/Scripts/HerdedHome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HerdedHome : MonoBehaviour
+{
+
+    Rigidbody2D rb; // Rigidbody physics component
+
+    public float closeenough = Mathf.Abs(1.0f);
+    public string targettag = "Finish";
+    Vector2 targetPosition = new Vector2 (0,0);
+
+    void Start()
+    {
+        // Get the Rigidbody component of this object
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get the target object with this tag
+        GameObject targetObject = GameObject.FindWithTag(targettag);
+
+        if (transform.position targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, movementSpeed * Time.deltaTime);
+        }
+
+    }
+}
