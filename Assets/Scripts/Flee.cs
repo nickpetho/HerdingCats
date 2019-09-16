c8ee128c1586fbd113e246526ff5f7a64ff045d4
Nick Petho <nick.petho@gmail.com>
2019-09-16 17:34:00 -0400

    Added special ability and catch funtion

--- Assets/Scripts/Flee.cs
+++ Assets/Scripts/Flee.cs
@@ -10,40 +10,60 @@ public class Flee : MonoBehaviour
 
     public string targettag = "Player";
     public float speed = 0.01f;
+    public bool isCaught = false;
+
+    KeyboardMove playerScript;
 
     void Start()
     {
         // Get the Rigidbody component of this object
         rb = GetComponent<Rigidbody2D>();
+        playerScript = GameObject.FindWithTag(targettag).GetComponent<KeyboardMove>();
     }
 
     // Move away from location of target object
     void Update()
     {
 
-        // Get the target object with this tag
-        GameObject targetObject = GameObject.FindWithTag(targettag);
-
-        // If object not found, this will be null. Exit to prevent error.
-        if (targetObject == null)
+        if (!isCaught)
         {
-            return;
+
+            // Get the target object with this tag
+            GameObject targetObject = GameObject.FindWithTag(targettag);
+
+            // If object not found, this will be null. Exit to prevent error.
+            if (targetObject == null)
+            {
+                return;
+            }
+
+            // Get its Rigidboy component so can get its position
+            Rigidbody2D targetRb = targetObject.GetComponent<Rigidbody2D>();
+
+            // Get difference between current and target location 
+            Vector2 delta = targetRb.position - rb.position;
+
+            // Normalize to 1 
+            delta.Normalize();
+
+            // Multiply be speed
+            delta = delta * speed;
+
+            // Use this to change position
+            if (playerScript.isCatModeActive)
+            {
+                rb.position += delta;
+
+            }
+            else
+            {
+                rb.position -= delta;
+            }
+
+            if (Mathf.Abs(transform.position.x) < 1 && Mathf.Abs(transform.position.y) < 1)
+            {
+                isCaught = true;
+            }
         }
-
-        // Get its Rigidboy component so can get its position
-        Rigidbody2D targetRb = targetObject.GetComponent<Rigidbody2D>();
-
-        // Get difference between current and target location 
-        Vector2 delta = targetRb.position - rb.position;
-
-        // Normalize to 1 
-        delta.Normalize();
-
-        // Multiply be speed
-        delta = delta * speed;
-
-        // Use this to change position
-        rb.position -= delta;
-
     }
 }
--- Assets/Scripts/KeyboardMove.cs
+++ Assets/Scripts/KeyboardMove.cs
@@ -2,45 +2,70 @@
 using System.Collections;
 
 // This script uses keyboard events to move the object.
-public class KeyboardMove : MonoBehaviour {
+public class KeyboardMove : MonoBehaviour
+{
 
-	public float speed = 0.05f;
-	Rigidbody2D rb;
+    public float speed = 0.05f;
+    public bool isCatModeActive = false;
+    public float catModeDuration = 3.0f;
+    Rigidbody2D rb;
+    float timer;
+    bool isAbilityUsed = false;
 
-	// Use this for initialization
-	void Start () {
-		// Get the Rigidbody component of this object
-		rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        // Get the Rigidbody component of this object
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-		// Amount to move in each dimension
-		float dx = 0;
-		float dy = 0;
-	
-		// Input.GetKey(key) returns true if that key is currently down
-		// KeyCode.keyname is used for keys that don'w correspond to an ASCII char
+    // Update is called once per frame
+    void Update()
+    {
 
-		// Move up
-		if (Input.GetKey ("w") || Input.GetKey (KeyCode.UpArrow)) {
-			dy = speed;
-		}
-		// Move down
-		if (Input.GetKey ("s") || Input.GetKey (KeyCode.DownArrow)) {
-			dy = -speed;
-		}
-		// Move left
-		if (Input.GetKey ("a") || Input.GetKey (KeyCode.LeftArrow)) {
-			dx = -speed;
-		}
-		// Move right
-		if (Input.GetKey ("d") || Input.GetKey (KeyCode.RightArrow)) {
-			dx = speed;
-		}
+        // Amount to move in each dimension
+        float dx = 0;
+        float dy = 0;
 
-	    // Move by that amount
-		rb.position += new Vector2(dx, dy);
-	}
+        // Input.GetKey(key) returns true if that key is currently down
+        // KeyCode.keyname is used for keys that don'w correspond to an ASCII char
+
+        // Move up
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            dy = speed;
+        }
+        // Move down
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            dy = -speed;
+        }
+        // Move left
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            dx = -speed;
+        }
+        // Move right
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            dx = speed;
+        }
+
+        // Move by that amount
+        rb.position += new Vector2(dx, dy);
+
+        if (Input.GetKeyDown("space") && !isAbilityUsed)
+        {
+            isAbilityUsed = true;
+            isCatModeActive = true;
+        }
+        if (isCatModeActive)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer > catModeDuration)
+        {
+            isCatModeActive = false;
+        }
+    }
 }
